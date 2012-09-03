dfb5407c9d6582b42df406caa05a3269e6d928c1
Nico Weber <nicolasweber@gmx.de>
2012-09-03 13:09:36 -0700

    intersection code now finds field, but misses first row

--- cv/find_corners.c
+++ cv/find_corners.c
@@ -8,7 +8,8 @@
 static int imin(int a, int b) { return a < b ? a : b; }
 static int imax(int a, int b) { return a > b ? a : b; }
 
-static bool intersect(float point[2], float l1[2], float l2[2]) {
+#include <stdio.h>
+static bool intersect(float point[2], const float l1[2], const float l2[2]) {
   float nx1 = cos(l1[0]);
   float ny1 = sin(l1[0]);
 
@@ -19,16 +20,15 @@ static bool intersect(float point[2], float l1[2], float l2[2]) {
   if (fabs(d) < 0.0001f)
     return false;
 
-  // fabs() due to mod 180 in line orientation.
-  point[0] = fabs((ny2 * l1[1] - ny1 * l2[1]) / d);
-  point[1] = fabs((nx1 * l2[1] - nx2 * l1[1]) / d);
+  point[0] = (ny2 * l1[1] - ny1 * l2[1]) / d;
+  point[1] = (nx1 * l2[1] - nx2 * l1[1]) / d;
 
   return true;
 }
 
 bool find_corners(graymap_t* graymap, float corners[4][2]) {
   const int kNumAngles = 360;
-  const int kNumRadii = graymap->h;
+  const int kNumRadii = graymap->h / 2;
 
   float kSin[kNumAngles];
   float kCos[kNumAngles];
@@ -47,8 +47,8 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
       for (int a = 0; a < kNumAngles; ++a) {
         float nx = kCos[a];
         float ny = kSin[a];
-        float r = fabs(nx*x + ny*y);
-        unsigned ri = r * (kNumRadii - 1) / kMaxRadius;
+        float r = (nx*x + ny*y);
+        unsigned ri = (r + kMaxRadius) * (kNumRadii - 1) / (2 * kMaxRadius);
         houghmap[ri*kNumAngles + a]++;
       }
     }
@@ -62,7 +62,7 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
     if (houghmap[i] > maxhough)
       maxhough = houghmap[i];
 
-#if 0
+#if 1
   graymap_t* alloc_graymap(int w, int h);
   void free_graymap(graymap_t*);
   bool save_graymap_to_pgm(const char* filename, const graymap_t*);
@@ -79,7 +79,7 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
     for (int ai = 0; ai < kNumAngles; ++ai, ++i) {
       if (houghmap[i] > 75 * maxhough / 100) {
         // Candidate! Find max in neighborhood, zero out remainder.
-        const int k = 31;
+        const int k = 21;
         unsigned best = houghmap[i], besta = ai, bestr = ri;
         for (int rd = imax(ri - k/2, 0);
              rd <= imin(ri + k/2, kNumRadii - 1);
@@ -97,7 +97,7 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
 
         // XXX: could use kCos / kSin
         float deg = besta * M_PI / kNumAngles;
-        float radius = bestr * kMaxRadius / (kNumRadii - 1);
+        float radius = bestr * 2 * kMaxRadius / (kNumRadii - 1) - kMaxRadius;
 
         if (!line_set[0]) {
           for (int j = 0; j < 2; ++j) {
@@ -106,6 +106,7 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
           }
           line_set[0] = true;
         } else {
+          // XXX fmod
           if (fabs(deg - lines[0][0]) < 10 * M_PI / 180) {
             if (radius > lines[1][1]) {
               lines[1][0] = deg;
@@ -129,6 +130,20 @@ bool find_corners(graymap_t* graymap, float corners[4][2]) {
       }
     }
   }
+
+#if 1
+  for (int i = 0; i < 4; ++i ) {
+    float nx = cos(lines[i][0]);
+    float ny = sin(lines[i][0]);
+    float r = lines[i][1];
+    for (int y = 0; y < graymap->h; ++y) {
+      for (int x = 0; x < graymap->w; ++x) {
+        if (fabs(x*nx + y*ny) < r)
+          graymap->data[y*graymap->w + x] *= (i + 1) * 17;
+      }
+    }
+  }
+#endif
 
 #if 1
   int printf(const char*, ...);
