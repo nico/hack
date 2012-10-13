06fdefad638b242aa5db361d98d54fff9ab72c17
Nico Weber <nicolasweber@gmx.de>
2012-10-13 23:16:31 +0900

    , is a sequence point

--- wpng/wpng.c
+++ wpng/wpng.c
@@ -7,8 +7,7 @@
 
 void wpng(int w, int h, const uint8_t* pix, FILE* f) {  // pix: rgba in memory
   uint32_t crc_table[256];
-  for (int n = 0; n < 256; n++) {
-    uint32_t c = n;
+  for (uint32_t n = 0, c = 0; n < 256; n++, c = n) {
     for (int k = 0; k < 8; k++) c = (c & 1) ? 0xedb88320L ^ (c >> 1) : c >> 1;
     crc_table[n] = c;
   }
