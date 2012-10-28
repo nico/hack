1d6216851939e5ee2cb138924e55332b74c905db
Nico Weber <nicolasweber@gmx.de>
2012-10-28 11:23:59 -0700

    convert bits-per-sample to short as required; write samples-per-pixel tag (still not functional)

--- wpng/wtiff.c
+++ wpng/wtiff.c
@@ -17,11 +17,13 @@ void wtiff(int w, int h, const uint8_t* pix, FILE* f) {  // pix: rgba in memory
   // 0x102: bits per sample (8,8,8,8 for rgba :-/)
   // 0x103: compression (1: none, default)
   // 0x106: photometric interpretation (short value 2 = rgb)
-  // 0x111: strip offsets, 0x116: rows per strip, 0x117: StripByteCounts
+  // 0x111: strip offsets,
+  // 0x115: samples per pixel
+  // 0x116: rows per strip, 0x117: StripByteCounts
   // 0x152: extrasamples. (2 = unpremul alpha)
 
   // 2 bytes number of fields, fields, offset to next ifd (0)
-  uint16_t n_fields = 8;
+  uint16_t n_fields = 9;
   fwrite(&n_fields, 2, 1, f);
 
   uint16_t tag, type, sval; uint32_t count, val;
@@ -34,17 +36,21 @@ void wtiff(int w, int h, const uint8_t* pix, FILE* f) {  // pix: rgba in memory
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
   fwrite(&count, 4, 1, f); fwrite(&val, 4, 1, f);
 
-  tag = 0x102; type = 1; count = 4; val = 0x08080808;  // bits per sample
+  tag = 0x102; type = 3; count = 4; val = 0x7a;  // bits per sample
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
-  fwrite(&count, 4, 1, f); fwrite(&val, 1, 4, f);
+  fwrite(&count, 4, 1, f); fwrite(&val, 4, 1, f);
 
   tag = 0x106; type = 3; count = 1; sval = 2;  // rgb
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
   fwrite(&count, 4, 1, f); fwrite(&sval, 2, 1, f); fwrite("\0\0", 1, 2, f);
 
-  tag = 0x111; type = 4; count = h; val = 0x6e; // strip offsets from file start
+  tag = 0x111; type = 4; count = h; val = 0x7a + 8; // strip offsets from file start
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
   fwrite(&count, 4, 1, f); fwrite(&val, 4, 1, f);
+
+  tag = 0x115; type = 3; count = 1; sval = 4;  // samples per pixel
+  fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
+  fwrite(&count, 4, 1, f); fwrite(&sval, 2, 1, f); fwrite("\0\0", 1, 2, f);
 
   tag = 0x116; type = 4; count = 1; val = 1;  // rows per strip
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
@@ -54,7 +60,7 @@ void wtiff(int w, int h, const uint8_t* pix, FILE* f) {  // pix: rgba in memory
 
   // "For each strip, the number of bytes in that strip after any compression"
   //tag = 0x117; type = 1; count = 1; val = w * 4;  // bytes per strip
-  tag = 0x117; type = 3; count = h; val = 0x6e + h*4;  // bytes per strip
+  tag = 0x117; type = 3; count = h; val = 0x7a + 8 + h*4;  // bytes per strip
   fwrite(&tag, 2, 1, f); fwrite(&type, 2, 1, f);
   fwrite(&count, 4, 1, f); fwrite(&val, 4, 1, f);
 
@@ -64,9 +70,15 @@ void wtiff(int w, int h, const uint8_t* pix, FILE* f) {  // pix: rgba in memory
 
   fwrite("\0\0\0\0", 1, 4, f);
 
-  // strip data
+  // bits per sample
+  sval = 8;
+  fwrite(&sval, 2, 1, f);
+  fwrite(&sval, 2, 1, f);
+  fwrite(&sval, 2, 1, f);
+  fwrite(&sval, 2, 1, f);
+  // strip offsets
   for (int y = 0; y < h; ++y) {
-    val = 0x6e + h*6 + y * w * 4; fwrite(&val, 4, 1, f);
+    val = 0x7a + 8 + h*6 + y * w * 4; fwrite(&val, 4, 1, f);
   }
   // strip sizes
   for (int y = 0; y < h; ++y) {
