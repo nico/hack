6f43d803c88692981792b494126936858d358786
Nico Weber <nicolasweber@gmx.de>
2013-10-06 21:35:58 -0700

    print energy loss

--- numa/int.py
+++ numa/int.py
@@ -11,9 +11,17 @@ def main():
   ax = -x / (r**3)
   ay = -y / (r**3)
 
-  # Allegedly increases precision:
+  # Allegedly increases precision (energy loss after 500 steps is just 1.15%
+  # instead of 5.54%):
   vx += 0.5 * eps * ax
   vy += 0.5 * eps * ay
+
+  U = 0.5 * 1 * ((vx - 0.5 * eps * ax) ** 2 + (vy - 0.5 * eps * ay) ** 2)
+  T = -1 / r
+  E = U + T
+  print E
+
+  E0 = E
 
   for i in range(500):
     print "%f\t%f" % (x, y)
@@ -24,6 +32,13 @@ def main():
     ay = -y / (r**3)
     vx += eps * ax
     vy += eps * ay
+
+    U = 0.5 * 1 * ((vx - 0.5 * eps * ax) ** 2 + (vy - 0.5 * eps * ay) ** 2)
+    T = -1 / r
+    E = U + T
+    #print E
+
+  print 'energy change: %f%%' % (100 * (E - E0) / E0)
 
 
 if __name__ == '__main__':
