a0a1de392a0a24f3da52f36ea7094edc8db163ff
Nico Weber <nicolasweber@gmx.de>
2014-02-02 19:23:21 -0800

    count scopes

--- envbench/hashlink.c
+++ envbench/hashlink.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,7 +33,46 @@ typedef struct {
   TokenType type;
   char* ident_text;
 } Token;
-void read_token(Input* input, Token* t);
+void read_token(Input* input, Token* t) {
+  if (feof(input->f)) {
+    t->type = kTokEof;
+    return;
+  }
+
+  int last_char = ' ';
+  while (isspace(last_char))
+    last_char = fgetc(input->f);
+
+  if (last_char == '{') {
+    t->type = kTokOpenScope;
+    return;
+  }
+  if (last_char == '}') {
+    t->type = kTokCloseScope;
+    return;
+  }
+  t->type = kTokIdent;
+}
+
+void parse(Input* input) {
+  int nesting_depth = 0;
+  Token token;
+  for (;;) {
+    read_token(input, &token);
+    switch (token.type) {
+      case kTokEof:
+        if (nesting_depth != 0)
+          fprintf(stderr, "unbalanced scopes (%d)\n", nesting_depth);
+        return;
+      case kTokOpenScope:
+        ++nesting_depth;
+        break;
+      case kTokCloseScope:
+        --nesting_depth;
+        break;
+    }
+  }
+}
 
 int main(int argc, char* argv[]) {
   for (int i = 1; i < argc; ++i) {
@@ -41,6 +81,7 @@ int main(int argc, char* argv[]) {
       fprintf(stderr, "failed to open %s\n", argv[i]);
       continue;
     }
+    parse(input);
     close_file(input);
   }
 }
