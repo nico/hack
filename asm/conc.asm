d7e46c626edcf3b1edae19c482d0b57c389403db
Nico Weber <nicolasweber@gmx.de>
2015-02-08 20:01:44 -0800

    turn conc into a hello world

--- asm/conc.asm
+++ asm/conc.asm
@@ -1,9 +1,24 @@
+// Based on
+// http://davidad.github.io/blog/2014/03/23/concurrency-primitives-in-intel-64-assembly/
+
 #define SYSCALL_EXIT      0x2000001
+#define SYSCALL_WRITE     0x2000004
 
 .intel_syntax
 
 .global start
 start:
+  mov rdi, 1
+  lea rsi, qword ptr [rip+str]  # [rip+str@GOTPCREL] for GOT instead of rip-rel
+  mov rdx, [rip+len]  # len instead of mylen because llvm.org/PR22511
+  mov rax, SYSCALL_WRITE
+  syscall
+
   mov rdi, 42
   mov rax, SYSCALL_EXIT
   syscall
+
+str:
+  .ascii "Hello, ASM.\n"
+  .set mylen, .-str  # See llvm.org/22511
+len: .quad . - str
