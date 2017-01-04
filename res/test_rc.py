fb0096289cb3d6c8096f249d358061fa28b51b2f
Nico Weber <nicolasweber@gmx.de>
2017-01-03 19:15:18 -0500

    rc: add python version of test script

---
+++ res/test_rc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import filecmp, subprocess, sys
+if sys.platform == 'win32':
+  cmd = 'cl rc.cc /EHsc /wd4838 /nologo'
+else:
+  cmd = 'clang++ -std=c++14 -o rc rc.cc -Wall -Wno-c++11-narrowing'
+subprocess.check_call(cmd)
+
+tests = [
+'cursor',
+'bitmap',
+'icon',
+'menu',
+'menu_opts',
+'dialog_nocontrols', 'dialog_controls',
+'dialogex_nocontrols', 'dialogex_controls',
+'stringtable',
+'accelerators',
+'rcdata',
+'versioninfo_fixedonly', 'versioninfo',
+'dlginclude',
+'html',
+'custom',
+'stringnames',
+]
+
+for test in tests:
+  print(test)
+  subprocess.check_call('rc.exe' if sys.platform == 'win32' else 'rc',
+                        stdin=open('test/%s.rc' % test))
+  assert filecmp.cmp('out.res', 'test/%s.res' % test)
+
+print('passed')
