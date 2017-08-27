71af4bf4101c961292a0aa2512e36b0793f3a6ab
Nico Weber <nicolasweber@gmx.de>
2017-08-27 18:13:49 -0400

    mc: add a few more tokens

--- mc/mc.py
+++ mc/mc.py
@@ -9,14 +9,46 @@ except ImportError:
   sys.path.insert(0, os.path.join('/src/chrome/src/third_party'))
   from ply import lex
 
+# https://msdn.microsoft.com/en-us/library/windows/desktop/dd996906(v=vs.85).aspx
 tokens = (
+  'COLON',
   'COMMENT',
+  'DOT',
+  'EQUAL',
+  'LPAREN',
+  'RPAREN',
+  'identifier',
+  'number',
+  # Header section keywords:
+  'kwFacilityNames',
+  'kwLanguageNames',
+  'kwMessageIdTypedef',
+  'kwOutputBase',
+  'kwSeverityNames',
+  # Message section keywords:
+  'kwFacility',
+  'kwLanguage',
+  'kwMessageId',
+  #'kwOutputBase' # Already present in header section.
+  'kwSeverity',
+  'kwSymbolicName',
   )
 
+t_COLON = ':'
 t_COMMENT = ';[^\n]*'
+t_DOT = '^.$'
+t_EQUAL = '='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_identifier = '[a-zA-Z_][a-zA-Z_0-9]*'
+t_number = '0[xX][0-9a-fA-F]+|[0-9]+'
+# Make token rules for all the tokens starting with 'kw':
+for t in tokens:
+  if t.startswith('kw'):
+    globals()['t_' + t] = t[len('kw'):]
 
-def t_newline(t):
-  r'\n+'
+def t_whitespace(t):
+  r'(?:(?:\r?\n)|[ \t])+'
   return None  # Ignore newlines.
 
 lexer = lex.lex()
