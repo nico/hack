1546390487e6e47af6ceb424ee4f9ed1cd398a6d
Nico Weber <nicolasweber@gmx.de>
2018-01-16 11:58:37 -0500

    gunzip.c: remove fixed FIXME (clang r322573)

--- cab/gunzip.c
+++ cab/gunzip.c
@@ -12,7 +12,7 @@ clang -O2 cab/gunzip.c
 
 static void fatal(const char* msg, ...) {
   va_list args;
-  va_start(args, msg);  // FIXME: add to clang's lib/clang/Basic/Builtins.def?
+  va_start(args, msg);
   vfprintf(stderr, msg, args);
   va_end(args);
   exit(1);
