276885565f0d122bcd26b78b14a9b907506e5d70
Nico Weber <thakis@chromium.org>
2019-12-08 17:05:24 -0500

    aoc yet more 8b golf

--- aoc/2019/8b.py
+++ aoc/2019/8b.py
@@ -3,10 +3,9 @@ import fileinput
 
 w, h = 25, 6
 line = list(fileinput.input())[0].rstrip()
-pic = ['2'] * (w * h)
-for layer in range(0, len(line), w*h):
-    for i in range(0, w*h):
-        if pic[i] == '2':
-            pic[i] = line[layer + i]
+pic = ['2'] * (w*h)
+for i in range(0, len(line)):
+    if pic[i % (w*h)] == '2':
+        pic[i % (w*h)] = line[i]
 for y in range(0, w*h, w):
     print(''.join(pic[y:y + w]).replace('0', ' '))
