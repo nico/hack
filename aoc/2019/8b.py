c48ae4b1f04092a08fa28ae9b8bb1507cac8c305
Nico Weber <thakis@chromium.org>
2019-12-08 10:13:41 -0500

    aoc day 8b better

--- aoc/2019/8b.py
+++ aoc/2019/8b.py
@@ -6,9 +6,8 @@ line = list(fileinput.input())[0].rstrip()
 layers = [line[i:i + w*h] for i in range(0, len(line), w*h)]
 pic = ['2'] * (w * h)
 for l in layers:
-    for y in range(0, h):
-        for x in range(0, w):
-            if pic[y*w + x] == '2':
-                pic[y*w + x] = l[y*w + x]
+    for i in range(0, w*h):
+        if pic[i] == '2':
+            pic[i] = l[i]
 for y in range(0, w*h, w):
     print(''.join(pic[y:y + w]).replace('0', ' '))
