c7c2016e92dbd42d0229256fd21da85cd461cc77
Nico Weber <thakis@chromium.org>
2020-01-06 08:02:42 -0500

    syncbot stats: put linux, mac, win all in one graph

--- llvmgnsyncbot/stats.py
+++ llvmgnsyncbot/stats.py
@@ -29,22 +29,42 @@ def json_dir_to_dataframe(json_dir):
     return pd.io.json.json_normalize([load(f) for f in files])
 
 
-df = json_dir_to_dataframe(sys.argv[1])
-
-# Only keep successful builds.
-df = df[df.exit_code == 0]
-
-df.start_utc = pd.to_datetime(df.start_utc, utc=True)
-#df.start_utc = df.start_utc.dt.tz_convert('US/Eastern')
-df = df.set_index('start_utc')
-
 # Register datetime converter for matplotlib plotting.
 pd.plotting.register_matplotlib_converters()
 
-df['elapsed_m'] = df.elapsed_s / 60.
-plt.scatter(df.index, df.elapsed_m, alpha=0.1)
-plt.xlabel('date')
-plt.ylabel('elapsed minutes')
+max_y = 0
+min_x = pd.to_datetime(pd.Timestamp.max, utc=True)
+max_x = pd.to_datetime(pd.Timestamp.min, utc=True)
 
-plt.plot(df.index, df.elapsed_m.rolling('2d').mean(), color='C1')
+for platform in ('linux', 'mac', 'win'):
+    df = json_dir_to_dataframe(os.path.join(sys.argv[1], platform))
+
+    # Only keep successful builds.
+    df = df[df.exit_code == 0]
+
+    # Builds before 137 had lots of security software enabled which caused
+    # cycle times of 45+ mins. That blows out the scale, so omit them.
+    if platform == 'win':
+        df = df[df.build_nr >= 137]
+
+    df.start_utc = pd.to_datetime(df.start_utc, utc=True)
+    #df.start_utc = df.start_utc.dt.tz_convert('US/Eastern')
+    df = df.set_index('start_utc')
+
+    max_x = max(max_x, df.index.max())
+    min_x = min(min_x, df.index.min())
+
+    df['elapsed_m'] = df.elapsed_s / 60.
+    max_y = max(max_y, df.elapsed_m.max())
+
+    plt.scatter(df.index, df.elapsed_m, alpha=0.1, label=platform)
+    plt.xlabel('date')
+    plt.ylabel('elapsed minutes')
+
+    #plt.plot(df.index, df.elapsed_m.rolling('2d').mean(),
+             #label='%s (avg)' % platform)
+
+plt.legend()
+plt.xlim(min_x, max_x)
+plt.ylim(0, max_y)
 plt.show()
