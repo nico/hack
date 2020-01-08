00de98bb98e22693bc57f926998187f36bc53e35
Nico Weber <thakis@chromium.org>
2020-01-08 13:56:39 -0500

    oauth2 script: add some profiling hooks

--- oauth2_print_goma_client_version.sh
+++ oauth2_print_goma_client_version.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # A small demo on how to do basic oauth2 with curl,
 # and how to do simple goma api calls.
+
+# https://stackoverflow.com/questions/5014823/how-to-profile-a-bash-shell-script-slow-startup
+# but with perl instead of %N since macOS date doesn't understand that, and
+# with sed's `-u` flag removed for the same reason.
+PROFILE=0
+if [ "$PROFILE" -eq "1" ]; then
+  PS4='+ $(perl -MTime::HiRes=time -e '"'"'printf "%.9f\n", time'"'"')\011 '
+  exec 3>&2 2>/tmp/bashstart.$$.log
+  set -x
+fi
 
 set -ue
 
@@ -25,11 +35,20 @@ token_json=$(curl -s --request POST \
   --data client_secret=$client_secret \
   --data refresh_token=$refresh_token \
   --data grant_type=refresh_token \
-https://accounts.google.com/o/oauth2/token)
+https://oauth2.googleapis.com/token)
+#https://accounts.google.com/o/oauth2/token)
 
 access_token=$(echo $token_json | jq -r .access_token)
 
+#curl -H "Authorization: Bearer $access_token" \
+    #https://oauth2.googleapis.com/tokeninfo
+
 current_url=$(curl -s -H "Authorization: Bearer $access_token" \
   https://clients5.google.com/cxx-compiler-service/downloadurl)
-
 curl -H "Authorization: Bearer $access_token" $current_url/MANIFEST
+
+
+if [ "$PROFILE" -eq "1" ]; then
+  set +x
+  exec 2>&3 3>&-
+fi
