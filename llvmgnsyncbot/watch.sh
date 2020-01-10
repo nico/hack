7317ae8721d4f5501f84cbcb3aeec3e6b96d71d1
Nico Weber <thakis@chromium.org>
2020-01-09 23:03:29 -0500

    gnsyncbot: make watch.sh record how long annotate.py takes to run

--- llvmgnsyncbot/watch.sh
+++ llvmgnsyncbot/watch.sh
@@ -1,17 +1,23 @@
 #/bin/bash
 
 # Use like
-# hack/llvmgnsyncbot/watch.sh buildlog html hack > watch.out 2> watch.err < /dev/null
+# hack/llvmgnsyncbot/watch.sh buildlog html hack > html/watch.out 2> html/watch.err < /dev/null
 # disown -h
 
 # FIXME maybe add -m flag for robustness
 # FIXME maybe add -d flag instead of the disown stuff above
 
 while inotifywait -e create -r $1; do
+  now=$(TZ=UTC date +%FT%TZ)
+  echo $now
+
   # Update annotation script.
   # FIXME: Maybe exec self if the pull updated watch.sh?
   (cd $3; git pull)
 
   # Create output.
+  SECONDS=0
   $3/llvmgnsyncbot/annotate.py $1 $2
+  echo $SECONDS > $2/watchtime.txt
+  echo $now $SECONDS >> $2/watchtimes.txt
 done
