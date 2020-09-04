4c36e52e16b288c8237c9e0abe8e982eedd72e1d
Nico Weber <thakis@chromium.org>
2020-09-04 13:39:17 -0400

    add rotn.py

---
+++ rotn.py
@@ -0,0 +1,7 @@
+import sys
+n = int(sys.argv[1])
+for line in sys.stdin:
+    for c in line:
+        if c.isalpha():
+            c = chr(((ord(c) & 0x1f) - 1 + n) % 26 + (ord(c) & 0xe0) + 1)
+        sys.stdout.write(c)
