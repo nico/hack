7701ce46c260c0024cb3a19d4790423f29b702fd
Nico Weber <thakis@chromium.org>
2020-12-08 10:02:25 -0500

    bench.py: reorder cmd validation

--- bench.py
+++ bench.py
@@ -20,15 +20,14 @@ parser.add_argument('-n', help='number of repetitions', default=5, type=int)
 parser.add_argument('--output', '-o', help='write timing output to this file')
 parser.add_argument('cmd', nargs=argparse.REMAINDER, help='command to time')
 args = parser.parse_args()
+
+if args.cmd and args.cmd[0] == '--': # Make `bench.py -o foo -- cmd` work
+    del args.cmd[0]
 if not args.cmd:
     parser.print_help()
     sys.exit(1)
 
 out = open(args.output, 'w') if args.output else sys.stdout
-
-# Make `bench.py -o foo -- cmd` work
-if args.cmd and args.cmd[0] == '--':
-    del args.cmd[0]
 
 subprocess.call(args.cmd)  # Warmup
 for _ in range(args.n):
