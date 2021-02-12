3a49e5e66b21d8cd3697ef2e58f399f59058a434
Nico Weber <thakis@chromium.org>
2021-02-12 10:05:18 -0500

    rotn: use args if present, else stdin

--- rotn.py
+++ rotn.py
@@ -2,7 +2,8 @@
 # Takes n from argv, then rot-n encodes stdin.
 import sys
 n = int(sys.argv[1])
-for line in sys.stdin:
+source = sys.stdin if len(sys.argv) <= 2 else [s + '\n' for s in sys.argv[2:]]
+for line in source:
     for c in line:
         if c.isalpha():
             c = chr(((ord(c) & 0x1f) - 1 + n) % 26 + (ord(c) & 0xe0) + 1)
