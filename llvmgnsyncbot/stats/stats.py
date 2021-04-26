a279511d3cc6c28de7f1e40f5a42b79edc44d1f2
Nico Weber <thakis@chromium.org>
2021-04-26 11:13:43 -0400

    gnsyncbot: put most of stats.py in a main() function
    no behavior change

--- llvmgnsyncbot/stats/stats.py
+++ llvmgnsyncbot/stats/stats.py
@@ -27,57 +27,62 @@ def json_dir_to_dataframe(json_dir):
     return pd.json_normalize([load(f) for f in files])
 
 
-# Register datetime converter for matplotlib plotting.
-pd.plotting.register_matplotlib_converters()
+def main():
+    # Register datetime converter for matplotlib plotting.
+    pd.plotting.register_matplotlib_converters()
 
-max_y = 0
-min_x = pd.to_datetime(pd.Timestamp.max, utc=True)
-max_x = pd.to_datetime(pd.Timestamp.min, utc=True)
+    max_y = 0
+    min_x = pd.to_datetime(pd.Timestamp.max, utc=True)
+    max_x = pd.to_datetime(pd.Timestamp.min, utc=True)
 
-for platform in ('linux', 'mac', 'macm1', 'win',):
-    df = json_dir_to_dataframe(os.path.join(sys.argv[1], platform))
+    for platform in ('linux', 'mac', 'macm1', 'win',):
+        df = json_dir_to_dataframe(os.path.join(sys.argv[1], platform))
 
-    # Only keep successful builds.
-    df = df[df.exit_code == 0]
+        # Only keep successful builds.
+        df = df[df.exit_code == 0]
 
-    df.start_utc = pd.to_datetime(df.start_utc, utc=True)
-    #df.start_utc = df.start_utc.dt.tz_convert('US/Eastern')
-    df = df.set_index('start_utc')
+        df.start_utc = pd.to_datetime(df.start_utc, utc=True)
+        #df.start_utc = df.start_utc.dt.tz_convert('US/Eastern')
+        df = df.set_index('start_utc')
 
-    if platform == 'win':
-        # Builds before 137 had lots of security software enabled which caused
-        # cycle times of 45+ mins. That blows out the scale, so omit them.
-        df = df[df.build_nr >= 137]
-        # Filter out shark fin, for clearer trend lines.
-        fin_start = pd.Timestamp('2019-11-08').tz_localize('UTC')
-        fin_end = pd.Timestamp('2019-12-05').tz_localize('UTC')
-        df = df[(df.elapsed_s <= 8 * 60) |
-                (df.index < fin_start) | (df.index > fin_end)]
+        if platform == 'win':
+            # Builds before 137 had lots of security software enabled which
+            # caused cycle times of 45+ mins. That blows out the scale, so omit
+            # them.
+            df = df[df.build_nr >= 137]
+            # Filter out shark fin, for clearer trend lines.
+            fin_start = pd.Timestamp('2019-11-08').tz_localize('UTC')
+            fin_end = pd.Timestamp('2019-12-05').tz_localize('UTC')
+            df = df[(df.elapsed_s <= 8 * 60) |
+                    (df.index < fin_start) | (df.index > fin_end)]
 
-    max_x = max(max_x, df.index.max())
-    min_x = min(min_x, df.index.min())
+        max_x = max(max_x, df.index.max())
+        min_x = min(min_x, df.index.min())
 
-    df['elapsed_m'] = df.elapsed_s / 60.
-    max_y = max(max_y, df.elapsed_m.max())
+        df['elapsed_m'] = df.elapsed_s / 60.
+        max_y = max(max_y, df.elapsed_m.max())
 
-    plt.scatter(df.index, df.elapsed_m, alpha=0.1, label=platform)
+        plt.scatter(df.index, df.elapsed_m, alpha=0.1, label=platform)
 
-    plt.plot(df.index, df.elapsed_m.rolling(
-            250, min_periods=10, win_type='hann', center=True).mean(),
-        label='%s (avg)' % platform)
+        plt.plot(df.index, df.elapsed_m.rolling(
+                250, min_periods=10, win_type='hann', center=True).mean(),
+            label='%s (avg)' % platform)
 
-plt.ylabel('elapsed minutes')
-plt.gcf().autofmt_xdate()
-plt.legend()
-plt.tight_layout()  # Less whitespace around the plot.
+    plt.ylabel('elapsed minutes')
+    plt.gcf().autofmt_xdate()
+    plt.legend()
+    plt.tight_layout()  # Less whitespace around the plot.
 
-plt.xlim(min_x, max_x)
-plt.ylim(0, max_y)
+    plt.xlim(min_x, max_x)
+    plt.ylim(0, max_y)
 
-# Highlight weekends in date range.
-s = pd.date_range(min_x.normalize(), max_x.normalize()).to_series()
-for d in s[s.dt.dayofweek >= 5]:
-    plt.gca().axvspan(d, d + pd.Timedelta(days=1),
-                      alpha=0.03, facecolor='black', edgecolor='none')
+    # Highlight weekends in date range.
+    s = pd.date_range(min_x.normalize(), max_x.normalize()).to_series()
+    for d in s[s.dt.dayofweek >= 5]:
+        plt.gca().axvspan(d, d + pd.Timedelta(days=1),
+                          alpha=0.03, facecolor='black', edgecolor='none')
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    sys.exit(main())
