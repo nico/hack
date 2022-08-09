f1c614eedb17506be6a466e23d642755aad30f34
Nico Weber <thakis@chromium.org>
2022-08-09 09:07:40 -0400

    aus: move test helpers out-of-line

--- aus/draw_line_test.cc
+++ aus/draw_line_test.cc
@@ -6,55 +6,20 @@
 struct TestSurface {
   TestSurface(size_t w, size_t h, const char* name) : fb{w, h}, name(name) {}
 
-  TestSurface& draw_line(int x1, int y1, int x2, int y2) {
-    ::draw_line(fb.surface(), x1, y1, x2, y2, kFill);
-    return *this;
-  }
-
-  void should_be(std::string raw_expected) {
-    std::string actual = fb_to_string();
-    std::string expected = filter_spaces(std::move(raw_expected));
-
-    if (actual == expected)
-      return;
-
-    fprintf(stderr, "Test '%s' failed.\n", name);
-    fprintf(stderr, "Expected:\n%s\n", expected.c_str());
-    fprintf(stderr, "Actual:\n%s\n", actual.c_str());
-  }
+  TestSurface& draw_line(int x1, int y1, int x2, int y2);
+  void should_be(std::string raw_expected);
 
 private:
   static constexpr Pixel kFill = rgb(255, 255, 255);
-
-  std::string fb_to_string() const {
-    std::string s;
-    for (int y = fb.height - 1; y >= 0; --y) {
-      for (int x = 0; x < fb.width; ++x)
-        s += fb.scanline(y)[x] == kFill ? '#' : '.';
-      if (y != 0)
-        s += '\n';
-    }
-    return s;
-  }
-
-  std::string filter_spaces(std::string s) {
-    std::string filtered;
-    for (char c : s)
-      if (c != ' ')
-        filtered += c;
-    return filtered;
-  }
+  std::string fb_to_string() const;
+  std::string filter_spaces(std::string s);
 
   Framebuffer fb;
   const char* name;
 };
 
-TestSurface test_surface(size_t w, size_t h, const char* name) {
-  return TestSurface{w, h, name};
-}
-
 int main() {
-  test_surface(9, 5, "octant 1").
+  TestSurface(9, 5, "octant 1").
     draw_line(4, 2, 4 + 4, 2 + 2).
     should_be(R"(.........
                  ......##.
@@ -62,4 +27,40 @@ int main() {
                  .........
                  .........)");
 
+}
+
+TestSurface& TestSurface::draw_line(int x1, int y1, int x2, int y2) {
+  ::draw_line(fb.surface(), x1, y1, x2, y2, kFill);
+  return *this;
+}
+
+void TestSurface::should_be(std::string raw_expected) {
+  std::string actual = fb_to_string();
+  std::string expected = filter_spaces(std::move(raw_expected));
+
+  if (actual == expected)
+    return;
+
+  fprintf(stderr, "Test '%s' failed.\n", name);
+  fprintf(stderr, "Expected:\n%s\n", expected.c_str());
+  fprintf(stderr, "Actual:\n%s\n", actual.c_str());
+}
+
+std::string TestSurface::fb_to_string() const {
+  std::string s;
+  for (int y = fb.height - 1; y >= 0; --y) {
+    for (int x = 0; x < fb.width; ++x)
+      s += fb.scanline(y)[x] == kFill ? '#' : '.';
+    if (y != 0)
+      s += '\n';
+  }
+  return s;
+}
+
+std::string TestSurface::filter_spaces(std::string s) {
+  std::string filtered;
+  for (char c : s)
+    if (c != ' ')
+      filtered += c;
+  return filtered;
 }
