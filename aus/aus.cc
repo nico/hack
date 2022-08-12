bcd3294ef91edf06bc87aaf1093330db91445ad4
Nico Weber <thakis@chromium.org>
2022-08-12 12:53:04 -0400

    aus: make two functions static

--- aus/aus.cc
+++ aus/aus.cc
@@ -2,7 +2,7 @@
 #include "framebuffer.h"
 
 // pix: bgra in memory, bottom-most scanline first
-void wtga(uint16_t w, uint16_t h, const uint8_t* pix, FILE* f) {
+static void wtga(uint16_t w, uint16_t h, const uint8_t* pix, FILE* f) {
   uint8_t wl = static_cast<uint8_t>(w), wh = static_cast<uint8_t>(w >> 8);
   uint8_t hl = static_cast<uint8_t>(h), hh = static_cast<uint8_t>(h >> 8);
   uint8_t head[] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, wl, wh, hl, hh, 32, 0};
@@ -10,7 +10,7 @@ void wtga(uint16_t w, uint16_t h, const uint8_t* pix, FILE* f) {
   fwrite(pix, 1, w * h * 4, f);
 }
 
-void write_tga(const char* name, Framebuffer& fb) {
+static void write_tga(const char* name, Framebuffer& fb) {
   FILE* f = fopen(name, "wb");
   if (!f) return;
 
