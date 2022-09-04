1014ad522645b11218ee629caf601e7654c70efd
Nico Weber <thakis@chromium.org>
2022-09-04 17:28:46 -0400

    add jpeg exif dumper skeleton
    These couple lines need a surprising amount of includes!

---
+++ jpeg_exif_dump.c
@@ -0,0 +1,46 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+static void fatal(const char* msg, ...) {
+  va_list args;
+  va_start(args, msg);
+  vfprintf(stderr, msg, args);
+  va_end(args);
+  exit(1);
+}
+
+static void dump(uint8_t* begin, uint8_t* end) {
+}
+
+int main(int argc, char* argv[]) {
+  const char* in_name = argv[0];
+
+  // Read input.
+  int in_file = open(in_name, O_RDONLY);
+  if (!in_file)
+    fatal("Unable to read \'%s\'\n", in_name);
+
+  struct stat in_stat;
+  if (fstat(in_file, &in_stat))
+    fatal("Failed to stat \'%s\'\n", in_name);
+
+  // Casting memory like this is not portable.
+  void* contents = mmap(
+      /*addr=*/0, in_stat.st_size,
+      PROT_READ, MAP_SHARED, in_file, /*offset=*/0);
+  if (contents == MAP_FAILED)
+    fatal("Failed to mmap: %d (%s)\n", errno, strerror(errno));
+
+  dump(contents, contents + in_stat.st_size);
+
+  munmap(contents, in_stat.st_size);
+  close(in_file);
+}
